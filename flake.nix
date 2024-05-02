5f0e107bac8910748b1304595094589bc9aef58b
Dalia Rojas Vallejo <daliarojasvallejo@gmail.com>
2024-05-01 20:36:16 -0500

    Inicializar repositorio

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Dependencias de desarrollo para hacer mi página web";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = import ./shell.nix {inherit pkgs;};
+  };
+}
